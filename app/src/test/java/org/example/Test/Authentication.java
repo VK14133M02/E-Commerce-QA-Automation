0df9d09f3fa24e76fe040f6431f5eeaa7b3de5ff
Vikram Kumar <vk14133m02059@gmail.com>
2024-07-13 00:05:39 +0530

    Added extent report

--- app/src/test/java/org/example/Test/Authentication.java
+++ app/src/test/java/org/example/Test/Authentication.java
@@ -5,16 +5,25 @@ import java.io.IOException;
 import java.util.Properties;
 
 import org.example.DriverSingleton;
-import org.example.ReportSingleton;
 import org.example.Pages.Home;
 import org.example.Pages.Login;
+import org.example.ReportSingleton;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 
 public class Authentication {
     WebDriver driver; 
+
+    ExtentReports report;
+    ExtentTest test;
 
     String mob;
     String pass;
@@ -32,15 +41,20 @@ public class Authentication {
 
         this.mob = prop.getProperty("MOB");
         this.pass = prop.getProperty("PASS");
+
+        ReportSingleton reportSingleton = ReportSingleton.getInstanceOfSingletonReport();
+        report = reportSingleton.getReport();
+        test = report.startTest("Authentication");
     }
 
     @Test(description = "Login with valid credential")
-    public void validLogin(){
+    public void validLogin() throws IOException{
         boolean status = false;
 
         Home home = new Home(driver);
         home.navigateToHomePage();
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.amazon.in/");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
 
         home.clickOnLoginButton();
         // System.out.println("The current URL is "+driver.getCurrentUrl());
@@ -50,22 +64,27 @@ public class Authentication {
         status = login.enterValidMobileNum(mob);
 
         Assert.assertTrue(status,"Not able to pass the mobile number");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
+
 
         status = login.enterValidPassword(pass);
 
         Assert.assertTrue(status,"Not able to pass the password");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
+
 
         home.signOut();
     }
 
 
     @Test(description = "Login with Invalid Credential")
-    public void inValidLogin(){
+    public void inValidLogin() throws IOException{
         boolean status = false;
 
         Home home = new Home(driver);
         home.navigateToHomePage();
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.amazon.in/");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
 
         home.clickOnLoginButton();
         // System.out.println("The current URL is "+driver.getCurrentUrl());
@@ -74,10 +93,15 @@ public class Authentication {
 
         status = login.enterInvalidMobileNum("0123456789");
         Assert.assertTrue(status,"Error in login with invlaid mobile number");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
+
     }
 
     @AfterSuite
     public void closeDriver(){
         driver.quit();        
+
+        report.endTest(test);
+        report.flush();
     } 
 }
--- app/src/test/java/org/example/Test/SearchProduct.java
+++ app/src/test/java/org/example/Test/SearchProduct.java
@@ -1,23 +1,39 @@
 package org.example.Test;
 
+import java.io.IOException;
+
 import org.example.DriverSingleton;
 import org.example.Pages.Home;
 import org.example.Pages.SearchResult;
+import org.example.ReportSingleton;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class SearchProduct {
     WebDriver driver; 
 
+    ExtentReports report;
+    ExtentTest test;
+
     @BeforeSuite(alwaysRun = true)
     public void createDriver(){
         DriverSingleton driverSingleton = new DriverSingleton();
-        driver = driverSingleton.getInstence();                  
+        driver = driverSingleton.getInstence();     
+        
+        ReportSingleton reportSingleton = ReportSingleton.getInstanceOfSingletonReport();
+        report = reportSingleton.getReport();
+        test = report.startTest("SearchProduct");
     }
 
     @Test(description = "Search for valid product")
-    public void searchValidProduct(){
+    public void searchValidProduct() throws IOException{
         boolean status = false;
 
         Home home = new Home(driver);
@@ -25,6 +41,8 @@ public class SearchProduct {
         home.navigateToHomePage();
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.amazon.in/");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
+
 
         SearchResult searchResult = new SearchResult(driver);
 
@@ -32,14 +50,18 @@ public class SearchProduct {
         status =  home.searchItem("laptop");
 
         Assert.assertTrue(status,"Not able to search the item");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
+
 
         status = searchResult.verifyValidSearchResult("Laptop");
 
         Assert.assertTrue(status,"Mismatch in search result");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
+
     }
 
     @Test(description = "Search for invalid product")
-    public void searchInvalidProduct(){
+    public void searchInvalidProduct() throws IOException{
         boolean status = false;
 
         Home home = new Home(driver);
@@ -47,6 +69,8 @@ public class SearchProduct {
         home.navigateToHomePage();
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.amazon.in/");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
+
 
         //search the Invalid item
         status = home.searchItem("oihgfr urhgoir ;hrgf sr uharfo huerf %# hrfor");
@@ -54,15 +78,22 @@ public class SearchProduct {
         Assert.assertTrue(status,"Not able to search the item");
 
         SearchResult searchResult = new SearchResult(driver);
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
+
 
         status = searchResult.verifyInvalidSearchResult("oihgfr urhgoir ;hrgf sr uharfo huerf %# hrfor");
 
         Assert.assertTrue(status,"Mismatch in search result");
+        test.log(LogStatus.INFO,"Screenshot: "+ test.addScreenCapture(ReportSingleton.captureScreenShot(driver)));
+
     }
 
     @AfterSuite
     public void closeDriver(){
-        driver.quit();        
+        driver.quit();       
+        
+        report.endTest(test);
+        report.flush();
     } 
 
 }
